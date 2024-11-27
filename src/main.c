1103f125127c325e784889ffdfa3c0a3ffac9fce
José Gabriel <jose@hivemedia.uk>
2024-11-27 16:57:06 -0300

    chore: remove default values

--- src/main.c
+++ src/main.c
@@ -1,13 +1,7 @@
 #include "programa.h"
 #include "tela.h"
-#include "empresa.h"
 
 int main() {
     Programa *programa = criar_programa();
-    Empresa *empresa = criar_empresa("UENP");
-    Funcionario *funcionario = criar_funcionario("carlos", 100, "Gerente");
-
-    inserir_funcionario(empresa, funcionario);
-    inserir_empresa(programa, empresa);
     perguntar_tela(programa);
 }
